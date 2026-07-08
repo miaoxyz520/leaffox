f6df9b799a4a135966ee027b0c8b201c98d8d312
shadow <miaoxyz520@163.com>
2026-07-08 17:59:43 +0800

    Delete Leaffox directory

--- Leaffox/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 管理员退出
- */
-require_once __DIR__ . '/../config.php';
-adminLog($db, '管理员退出', 'admin', $_SESSION['admin_id'] ?? 0);
-session_destroy();
-redirect('./index.php');
--- Leaffox/user/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * 用户退出
- */
-require_once __DIR__ . '/../config.php';
-session_destroy();
-redirect('./index.php');
